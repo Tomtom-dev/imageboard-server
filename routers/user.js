0d816c7cc801e57d6f0e0653175477e0bcdae01e
Tomtom-dev <thomasheddadji0@gmail.com>
2020-05-27 23:46:09 +0200

    initiate seeds

---
+++ routers/user.js
@@ -0,0 +1,35 @@
+const {Router} = require("express");
+const User = require("../models").user;
+
+const router= new Router()
+
+router.get("/users", async(req,res,next)=>{
+    try{
+        console.log("request received");
+        const allUsers= await User.findAll();
+        console.log("user fetched from db", allUsers);
+        res.json(allUsers)
+    }catch (error){
+        next(error);
+    }
+})
+
+router.post("/", async (req, res, next) => {
+    try {
+      const { email, password, fullName } = req.body;
+      if (!email || !password || !fullName) {
+        res.status(400).send("missing parameters");
+      } else {
+        const newUser = await User.create({
+          email,
+          password: bcrypt.hashSync(password, 10),
+          fullName
+        });
+        res.json(newUser);
+      }
+    } catch (e) {
+      next(e);
+    }
+  });
+
+module.exports= router;
